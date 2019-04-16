318bfd41f2c7be2919eb0357b85b9a42afe1c777
Karsten Knese <Karsten1987@users.noreply.github.com>
2019-04-16 15:13:09 -0500

    Confbot driver actionserver (#20)
    * initial commit confbot action server
    
    * working action server and client example
    
    * address review comments
    
    Signed-off-by: Karsten Knese <karsten@openrobotics.org>
    
    * use linters correctly
    
    Signed-off-by: Karsten Knese <karsten@openrobotics.org>
    
    * use duration instead of seconds
    
    Signed-off-by: Karsten Knese <karsten@openrobotics.org>
    
    * actually use linters (#21)
    
    Signed-off-by: Mikael Arguedas <mikael.arguedas@gmail.com>
    
    * action client has precedence over subscription
    
    Signed-off-by: Karsten Knese <karsten@openrobotics.org>
    
    * add builtin_interfaces to msg deps
    
    Signed-off-by: Karsten Knese <karsten@openrobotics.org>
    
    * rename goal_handle action state
    
    Signed-off-by: Karsten Knese <karsten@openrobotics.org>

--- confbot_driver/include/confbot_driver/confbot_driver.hpp
+++ confbot_driver/include/confbot_driver/confbot_driver.hpp
@@ -18,12 +18,16 @@
 #include <chrono>
 #include <cstdio>
 #include <memory>
+#include <mutex>
 #include <string>
 #include <cmath>
+
+#include "confbot_msgs/action/move_command.hpp"
 
 #include "geometry_msgs/msg/twist.hpp"
 
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
 
 #include "tf2_ros/static_transform_broadcaster.h"
 
@@ -57,24 +61,45 @@ struct RobotPosition
 
 class ConfbotDriver : public rclcpp::Node
 {
+  using MoveCommand = confbot_msgs::action::MoveCommand;
+  using ServerGoalHandle = rclcpp_action::ServerGoalHandle<MoveCommand>;
+
 public:
   ConfbotDriver()
-  : Node("confbot_driver")
+  : Node("confbot_driver"), cmd_vel_lock_(cmd_vel_mutex_, std::defer_lock)
   {}
 
   void init()
   {
-    rclcpp::Clock::SharedPtr clock = std::make_shared<rclcpp::Clock>(RCL_ROS_TIME);
-    msg_.header.stamp = clock->now();
+    clock_ = rclcpp::Clock(RCL_ROS_TIME);
+    msg_.header.stamp = clock_.now();
     msg_.header.frame_id = "odom";
     msg_.child_frame_id = "base_link";
 
     tf_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(shared_from_this());
-    timer_ = this->create_wall_timer(
+    odometry_timer_ = this->create_wall_timer(
       std::chrono::milliseconds(100), std::bind(&ConfbotDriver::update_odometry, this));
 
     cmd_vel_subscriber_ = this->create_subscription<geometry_msgs::msg::Twist>(
-      "cmd_vel", std::bind(&ConfbotDriver::update_position, this, std::placeholders::_1));
+      "cmd_vel", std::bind(&ConfbotDriver::update_velocity, this, std::placeholders::_1));
+
+    action_server_ = rclcpp_action::create_server<MoveCommand>(
+      shared_from_this(),
+      "move_command",
+      std::bind(
+        &ConfbotDriver::handle_goal, this, std::placeholders::_1, std::placeholders::_2),
+      std::bind(
+        &ConfbotDriver::handle_cancel, this, std::placeholders::_1),
+      std::bind(
+        &ConfbotDriver::handle_accepted, this, std::placeholders::_1));
+  }
+
+protected:
+  void reset_speed()
+  {
+    vel_lin_ = 0.0f;
+    vel_ang_ = 0.0f;
+    distance_traveled_ = 0.0f;
   }
 
   void update_odometry()
@@ -87,22 +112,100 @@ public:
     tf_broadcaster_->sendTransform(msg_);
   }
 
-  void update_position(std::shared_ptr<geometry_msgs::msg::Twist> twist_msg)
+  void update_velocity(std::shared_ptr<geometry_msgs::msg::Twist> twist_msg)
   {
-    vel_lin_ = twist_msg->linear.x;
-    vel_ang_ = twist_msg->angular.z;
+    auto sub_lock = std::unique_lock<std::mutex>(cmd_vel_mutex_, std::try_to_lock);
+    if (sub_lock.owns_lock()) {
+      vel_lin_ = twist_msg->linear.x;
+      vel_ang_ = twist_msg->angular.z;
+    }
+  }
+
+  rclcpp_action::GoalResponse handle_goal(
+    const std::array<uint8_t, 16> & uuid, std::shared_ptr<const MoveCommand::Goal> goal)
+  {
+    RCLCPP_INFO(
+      get_logger(),
+      "Got goal request with duration %d, linear velocity %f and angular velocity %f",
+      goal->duration, goal->linear_velocity, goal->angular_velocity);
+    (void)uuid;
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+  }
+
+  rclcpp_action::CancelResponse handle_cancel(
+    const std::shared_ptr<ServerGoalHandle> goal_handle)
+  {
+    RCLCPP_INFO(get_logger(), "Got request to cancel goal");
+    (void)goal_handle;
+    reset_speed();
+    return rclcpp_action::CancelResponse::ACCEPT;
+  }
+
+  void update_feedback(
+    const std::shared_ptr<ServerGoalHandle> goal_handle)
+  {
+    distance_traveled_ += vel_lin_;
+
+    const auto goal = goal_handle->get_goal();
+    auto result = std::make_shared<MoveCommand::Result>();
+
+    auto now = clock_.now();
+    auto time_elapsed = now - action_start_time_;
+    if (time_elapsed >= goal->duration) {
+      result->distance_traveled = distance_traveled_;
+      goal_handle->succeed(result);
+      feedback_timer_->cancel();
+      reset_speed();
+      cmd_vel_lock_.unlock();
+      return;
+    }
+
+    auto feedback = std::make_shared<MoveCommand::Feedback>();
+    feedback->distance_traveled = distance_traveled_;
+    feedback->time_elapsed = time_elapsed;
+
+    // Publish feedback
+    goal_handle->publish_feedback(feedback);
+    RCLCPP_INFO(get_logger(), "Publish Feedback");
+  }
+
+  void handle_accepted(const std::shared_ptr<ServerGoalHandle> goal_handle)
+  {
+    std::function<void()> fnc =
+      std::bind(&ConfbotDriver::update_feedback, this, goal_handle);
+
+    cmd_vel_lock_.lock();
+
+    feedback_timer_ = this->create_wall_timer(
+      std::chrono::milliseconds(1000), fnc);
+
+    vel_lin_ = goal_handle->get_goal()->linear_velocity;
+    vel_ang_ = goal_handle->get_goal()->angular_velocity;
+
+    action_start_time_ = clock_.now();
   }
 
 private:
   float vel_lin_ = 0.0f;
   float vel_ang_ = 0.0f;
+  float distance_traveled_ = 0.0f;
 
   geometry_msgs::msg::TransformStamped msg_;
   RobotPosition robot_position_;
 
   std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_broadcaster_;
-  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::TimerBase::SharedPtr odometry_timer_;
+
   std::shared_ptr<rclcpp::Subscription<geometry_msgs::msg::Twist>> cmd_vel_subscriber_;
+
+  std::shared_ptr<rclcpp_action::Server<MoveCommand>> action_server_;
+  rclcpp::Clock clock_;
+
+  rclcpp::TimerBase::SharedPtr feedback_timer_;
+  rclcpp::Time action_start_time_;
+
+  std::mutex cmd_vel_mutex_;
+  std::unique_lock<std::mutex> cmd_vel_lock_;
 };
 
 }  // namespace confbot_driver
---
+++ confbot_driver/src/confbot_actionclient.cpp
@@ -0,0 +1,107 @@
+// Copyright 2019 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <inttypes.h>
+#include <memory>
+
+#include "confbot_msgs/action/move_command.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
+using MoveCommand = confbot_msgs::action::MoveCommand;
+
+void feedback_callback(
+  rclcpp_action::ClientGoalHandle<MoveCommand>::SharedPtr,
+  const std::shared_ptr<const MoveCommand::Feedback> feedback,
+  rclcpp::Logger logger)
+{
+  RCLCPP_INFO(
+    logger, "Confbot traveled %f so far in %d seconds",
+    feedback->distance_traveled, feedback->time_elapsed.sec);
+}
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("confbot_actionclient");
+  auto action_client = rclcpp_action::create_client<MoveCommand>(node, "move_command");
+
+  if (!action_client->wait_for_action_server(std::chrono::seconds(10))) {
+    RCLCPP_ERROR(node->get_logger(), "Action server not available after waiting");
+    return 1;
+  }
+
+  // Populate a goal
+  auto goal_msg = MoveCommand::Goal();
+  goal_msg.duration.sec = 10;
+  goal_msg.duration.nanosec = 0u;
+  goal_msg.linear_velocity = 0.1;
+  goal_msg.angular_velocity = 0.1;
+
+  RCLCPP_INFO(node->get_logger(), "Sending goal");
+
+  // workaround to pass more objects, i. e. the logger, to the function callback
+  auto fcn = std::bind(
+    feedback_callback, std::placeholders::_1, std::placeholders::_2, node->get_logger());
+  auto goal_handle_future = action_client->async_send_goal(goal_msg, fcn);
+
+  if (rclcpp::spin_until_future_complete(node, goal_handle_future) !=
+    rclcpp::executor::FutureReturnCode::SUCCESS)
+  {
+    RCLCPP_ERROR(node->get_logger(), "send goal call failed :(");
+    return 1;
+  }
+
+  rclcpp_action::ClientGoalHandle<MoveCommand>::SharedPtr goal_handle = goal_handle_future.get();
+  if (goal_handle == nullptr) {
+    RCLCPP_ERROR(node->get_logger(), "Goal was rejected by server");
+    return 1;
+  }
+
+  // Wait for the server to be done with the goal
+  auto result_future = goal_handle->async_result();
+
+  RCLCPP_INFO(node->get_logger(), "Waiting for result");
+  if (rclcpp::spin_until_future_complete(node, result_future) !=
+    rclcpp::executor::FutureReturnCode::SUCCESS)
+  {
+    RCLCPP_ERROR(node->get_logger(), "get result call failed :(");
+    return 1;
+  }
+
+  rclcpp_action::ClientGoalHandle<MoveCommand>::WrappedResult wrapped_result = result_future.get();
+
+  switch (wrapped_result.code) {
+    case rclcpp_action::ResultCode::SUCCEEDED:
+      break;
+    case rclcpp_action::ResultCode::ABORTED:
+      RCLCPP_ERROR(node->get_logger(), "Goal was aborted");
+      return 1;
+    case rclcpp_action::ResultCode::CANCELED:
+      RCLCPP_ERROR(node->get_logger(), "Goal was canceled");
+      return 1;
+    default:
+      RCLCPP_ERROR(node->get_logger(), "Unknown result code");
+      return 1;
+  }
+
+  RCLCPP_INFO(node->get_logger(), "result received");
+  RCLCPP_INFO(
+    node->get_logger(), "robot traveled %f meters", wrapped_result.result->distance_traveled);
+
+  action_client.reset();
+  rclcpp::shutdown();
+  return 0;
+}
