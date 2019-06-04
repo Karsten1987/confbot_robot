96053135361ede155441c4de8e335185dc5c1f51
Mikael Arguedas <mikael.arguedas@gmail.com>
2019-06-04 16:34:08 +0200

    register confbot_tools in index
    Signed-off-by: Mikael Arguedas <mikael.arguedas@gmail.com>
    
    missing resource file
    
    Signed-off-by: Mikael Arguedas <mikael.arguedas@gmail.com>

--- confbot_tools/setup.py
+++ confbot_tools/setup.py
@@ -8,6 +8,8 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
@@ -16,14 +18,14 @@ setup(
     author_email='karsten.knese@googlemail.com',
     maintainer='Karsten Knese',
     maintainer_email='karsten.knese@googlemail.com',
-    keywords=['confbot'],
+    keywords=['confbot', 'ROS'],
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
-    license='Apache 2.0',
+    license='Apache License, Version 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
